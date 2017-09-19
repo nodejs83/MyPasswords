e8ea34d61fc92e1c2cde18ea862c4b5116115b5f
a602256 <jamsn83@gmail.com>
2017-09-19 12:06:00 +0100

    commit

--- app/src/main/java/com/hfad/mypasswords/CredentialDetailsActivity.java
+++ app/src/main/java/com/hfad/mypasswords/CredentialDetailsActivity.java
@@ -1,20 +1,21 @@
 package com.hfad.mypasswords;
 
+import android.app.*;
 import android.os.Bundle;
 
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.hfad.mypasswords.data.*;
 
 import java.sql.SQLException;
 
-public class CredentialDetailsActivity extends BaseActivity {
+public class CredentialDetailsActivity extends BaseActivity implements DialogFragment.DialogListener{
 
 
-
-
-
+    private Item itemObject = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +24,23 @@ public class CredentialDetailsActivity extends BaseActivity {
 
         Integer id = (Integer)getIntent().getExtras().get(Utils.ITEM_ID);
         Integer groupId = (Integer)getIntent().getExtras().get(Utils.GROUPID);
-        Item item = null;
         try{
-            item = getHelper().getItemDao().queryForId(id);
+            itemObject = getHelper().getItemDao().queryForId(id);
         }catch(SQLException e){
             e.printStackTrace();
         }
 
         TextView login = (TextView)findViewById(R.id.login);
-        login.setText(item.getLogin());
+        login.setText(itemObject.getLogin());
 
         TextView password = (TextView)findViewById(R.id.password);
+        password.setText("******");
+    }
 
-
-        try {
-            password.setText(EncUtil.decryptData(item.password));
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        //password.setText(item.getPassword());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_display, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -50,8 +49,22 @@ public class CredentialDetailsActivity extends BaseActivity {
             case android.R.id.home:
                 finish();
                 return true;
+            case R.id.action_show_pwd:
+                android.app.DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getFragmentManager(), "DialogFragment");
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onDialogPositiveClick(DialogFragment dialog){
+        TextView password = (TextView)findViewById(R.id.password);
+        try {
+            password.setText(EncUtil.decryptData(itemObject.getPassword()));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
